a56bdc4f5ce898099eb9197892ae0a411880c864
bilal shakeel <120062035+bilal2134@users.noreply.github.com>
2024-08-29 16:44:43 +0500

    Add files via upload

---
+++ Data/PMSDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMS.API.Models;
+
+
+namespace PMS.API.Data
+{
+    public class PMSDbContext : DbContext
+    {
+        public PMSDbContext(DbContextOptions options) : base(options) { }
+
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnType("uuid");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace PMS.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }    
+
+        public string Type { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
